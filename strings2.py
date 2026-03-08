1abf586850641b7b3559e3df7b5d5bf4d9f7be5d
Krish-01-code <krishkushwaha.w@gmail.com>
2026-03-08 00:35:51 -0600

    String slicing

---
+++ strings2.py
@@ -0,0 +1,22 @@
+# Strings Slicing/Indexing
+# Basically accessing specific elements of an list or string
+
+phone_num = "605-671-8620"
+
+#To print all the strings
+print(phone_num[0:])
+
+#To print first five:
+print(phone_num[0:6]) #last limit is exclsive
+
+#first to last with 2 steps
+print(phone_num[0::2])
+
+#Last element
+print(phone_num[-1])
+
+#Reverse
+print(phone_num[::-1])
+
+#Last five
+print(phone_num[-4::1])
